132c08ff055750c1ca4314303767f98e7d4a4771
David Robinson <david.pl.robinson@googlemail.com>
2018-07-23 13:17:23 +0000

    added docker compose

---
+++ git_set_remote.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "David Robinson"
+git config --global user.email "david.pl.robinson@googlemail.com"
+git config --global credential.helper store
+git config --global push.default matching
+git remote add scripts https://github.com/davidplrobinson/gitlab.git
